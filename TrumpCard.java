6b856a9bac6a0ca980ee6d83b3621e215e19bc9f
Tanujshahh <tanujtushar.shah@my.jcu.edu.au>
2017-09-10 17:24:41 +0800

    Final Trump card day 5

---
+++ TrumpCard.java
@@ -0,0 +1,13 @@
+
+public class TrumpCard extends Card{
+
+    public TrumpCard(String title, String economicValue){
+        super(title, economicValue);
+    }
+
+    public String toString(){
+
+        return "Card Name: " + cardTitle + " Category: " + cardEconomicValue + "\n";
+
+    }
+}
