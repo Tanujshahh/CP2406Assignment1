1d733b280d0f26c85d25ea3f590eef33ac9d1af5
Tanujshahh <tanujtushar.shah@my.jcu.edu.au>
2017-09-10 17:38:34 +0800

    Add files via upload

---
+++ MineralCard.java
@@ -0,0 +1,20 @@
+
+public class MineralCard extends Card {
+
+    public MineralCard(String title, String chemistry, String classification,
+                       String crystalSystem, String occurrence, double hardness,
+                       double specificGravity, String cleavage, String crystalAbundance,
+                       String economicValue) {
+        super(title, chemistry, classification, crystalSystem, occurrence, hardness, specificGravity,
+                cleavage, crystalAbundance, economicValue);
+    }
+
+    public String toString(){
+
+        return "Card Name: " + cardTitle + " , Chemistry: " + cardChemistry + " , Classification: " + cardClassification + " , Crystal System: " + cardCrystalSystem +
+                " , Occurrance: " + cardOccurrence + " , Hardness: " + cardHardness + " , Specific Gravity: " + cardSpecificGravity
+                + " , Cleavage: " + cardCleavage + " , Crustal Abundance: " + cardCrustalAbundance + " , Economic Value: " + cardEconomicValue + "\n";
+
+    }
+
+}
